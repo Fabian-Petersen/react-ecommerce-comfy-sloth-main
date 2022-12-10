1e3e55b8146e7c641090ad12ca293cb748f57379
Fabian Petersen <fabian@webrus.co.za>
2022-12-10 15:45:43 +0200

    completed-pages:'contact-hero-services-pagehero-about-checkout-error-and-homepage'

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,27 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+  return (
+    <main>
+      <PageHero title="about" />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="nice desk" />
+        <article className="title">
+          <h2>Our Story</h2>
+          <div className="underline"></div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+            saepe cum debitis! Nesciunt beatae sequi eos. Molestiae beatae
+            ducimus aliquam?
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -33,5 +49,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;
